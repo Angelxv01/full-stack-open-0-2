e5b5818ba77dc21106c75bc8b01ae661f347d15e
Angel <angeloxv@outlook.it>
2021-07-07 11:49:20 +0200

    part3: 7-8

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-const { response } = require("express");
+const morgan = require("morgan");
 const express = require("express");
 
 const app = express();
 
 app.use(express.json());
+app.use(
+  morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      JSON.stringify(req.body),
+    ].join(" ");
+  })
+);
 
 let people = [
   {
@@ -84,7 +98,7 @@ app.delete("/api/persons/:id", (req, res) => {
   res.status(204).end();
 });
 
-app.get("*", (req, res) => res.status(404).send("invalid route"));
+app.use((req, res) => res.status(404).send("invalid route"));
 
 const PORT = 3001;
 
