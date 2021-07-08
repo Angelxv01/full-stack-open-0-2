e47022744309127262da6c54a98e0d74437fac2a
Angel <angeloxv@outlook.it>
2021-07-08 08:37:38 +0200

    Remove dev dependency from code

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const morgan = require("morgan");
+// const morgan = require("morgan");
 const express = require("express");
 const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
-app.use(
-  morgan(function (tokens, req, res) {
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, "content-length"),
-      "-",
-      tokens["response-time"](req, res),
-      "ms",
-      JSON.stringify(req.body),
-    ].join(" ");
-  })
-);
+// app.use(
+//   morgan(function (tokens, req, res) {
+//     return [
+//       tokens.method(req, res),
+//       tokens.url(req, res),
+//       tokens.status(req, res),
+//       tokens.res(req, res, "content-length"),
+//       "-",
+//       tokens["response-time"](req, res),
+//       "ms",
+//       JSON.stringify(req.body),
+//     ].join(" ");
+//   })
+// );
 
 let people = [
   {
