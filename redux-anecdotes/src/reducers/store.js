8d794cdd4900b9bfb09b417f7fdfc32c7b1833ed
Angel <angeloxv@outlook.it>
2021-08-02 18:40:12 +0200

    part6: until 6.10

---
+++ redux-anecdotes/src/reducers/store.js
@@ -0,0 +1,14 @@
+import { createStore, combineReducers } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+import anecdoteReducer from './anecdoteReducer'
+import notificationReducer from './notificationReducer'
+
+const reducer = combineReducers({
+  anecdote: anecdoteReducer,
+  notification: notificationReducer
+})
+
+const store = createStore(reducer, composeWithDevTools())
+
+export default store
