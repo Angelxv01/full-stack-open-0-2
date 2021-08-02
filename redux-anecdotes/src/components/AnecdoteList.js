4fdda7a2122ff97e7fd50685567c31b737a3a88f
Angel <angeloxv@outlook.it>
2021-08-02 18:53:22 +0200

    part6: 6.11

--- redux-anecdotes/src/components/AnecdoteList.js
+++ redux-anecdotes/src/components/AnecdoteList.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { voteAnecdote } from '../reducers/anecdoteReducer'
+import {
+  setNotification,
+  resetNotification
+} from '../reducers/notificationReducer'
 
 const AnecdoteList = () => {
   const anecdotes = useSelector((state) => state.anecdote)
@@ -8,6 +12,9 @@ const AnecdoteList = () => {
 
   const vote = (id) => {
     dispatch(voteAnecdote(id))
+    const anecdote = anecdotes.find((obj) => obj.id === id)
+    dispatch(setNotification(`you voted ${anecdote.content}`))
+    setTimeout(() => dispatch(resetNotification()), 5000)
   }
   return (
     <div>
@@ -18,7 +25,13 @@ const AnecdoteList = () => {
             <div>{anecdote.content}</div>
             <div>
               has {anecdote.votes}
-              <button onClick={() => vote(anecdote.id)}>vote</button>
+              <button
+                onClick={() => {
+                  vote(anecdote.id)
+                }}
+              >
+                vote
+              </button>
             </div>
           </div>
         ))}
--- redux-anecdotes/src/reducers/notificationReducer.js
+++ redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,11 +1,22 @@
-const reducer = (state = 'hello', action) => {
+const reducer = (state = null, action) => {
   console.log('state now: ', state)
   console.log('action: ', action)
 
   switch (action.type) {
+    case 'SET':
+      return action.body.message
+    case 'RESET':
+      return null
     default:
       return state
   }
 }
+
+export const setNotification = (message) => ({
+  type: 'SET',
+  body: { message }
+})
+
+export const resetNotification = () => ({ type: 'RESET' })
 
 export default reducer
