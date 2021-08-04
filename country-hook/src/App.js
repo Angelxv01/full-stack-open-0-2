daf0216bd2679d74a212cdaa209721977c27bfb6
Angel <angeloxv@outlook.it>
2021-08-04 16:59:20 +0200

    part7: 7.7

--- country-hook/src/App.js
+++ country-hook/src/App.js
@@ -17,8 +17,15 @@ const useField = (type) => {
 
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
-
-  useEffect(() => {})
+  const url = `https://restcountries.eu/rest/v2/name/${name}?fullText=true`
+  useEffect(() => {
+    axios
+      .get(url)
+      .then(({ data }) => {
+        setCountry({ found: true, data: data[0] })
+      })
+      .catch((e) => setCountry({ found: false }))
+  }, [name])
 
   return country
 }
@@ -29,19 +36,19 @@ const Country = ({ country }) => {
   }
 
   if (!country.found) {
-    return (
-      <div>
-        not found...
-      </div>
-    )
+    return <div>not found...</div>
   }
 
   return (
     <div>
       <h3>{country.data.name} </h3>
       <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <div>population {country.data.population}</div>
+      <img
+        src={country.data.flag}
+        height="100"
+        alt={`flag of ${country.data.name}`}
+      />
     </div>
   )
 }
